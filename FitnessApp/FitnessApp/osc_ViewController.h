cbe675c6c2447631206c03e68f2dad3e3149ed90
Balkis Khouni <balkis.khouni@gmail.com>
2014-07-08 09:53:35 +0200

    First commit

---
+++ FitnessApp/FitnessApp/osc_ViewController.h
@@ -0,0 +1,13 @@
+//
+//  osc_ViewController.h
+//  FitnessApp
+//
+//  Created by MAC-04 on 08/07/2014.
+//  Copyright (c) 2014 MAC-04. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface osc_ViewController : UIViewController
+
+@end
---
+++ FitnessApp/FitnessApp/osc_ViewController.m
@@ -0,0 +1,29 @@
+//
+//  osc_ViewController.m
+//  FitnessApp
+//
+//  Created by MAC-04 on 08/07/2014.
+//  Copyright (c) 2014 MAC-04. All rights reserved.
+//
+
+#import "osc_ViewController.h"
+
+@interface osc_ViewController ()
+
+@end
+
+@implementation osc_ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
